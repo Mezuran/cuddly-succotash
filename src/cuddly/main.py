fb69f3eacee4f10b85193b5f65ef56eaa58dd077
Mitho Mizuno <mithomizuno@gmail.com>
2026-01-24 20:56:08 +0700

    Fully Functional Streamlit App

--- src/cuddly/main.py
+++ src/cuddly/main.py
@@ -1,12 +1,12 @@
 import sys
-import os
+from pathlib import Path
 from streamlit.web import cli as stcli
 
 def run():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    ui_path = os.path.join(current_dir, "ui.py")
+    current_dir = Path(__file__).parent
+    ui_path = current_dir / "ui.py"
 
-    sys.argv = ["streamlit", "run", ui_path]
+    sys.argv = ["streamlit", "run", str(ui_path)]
     sys.exit(stcli.main())
 
 if __name__ == "__main__":
